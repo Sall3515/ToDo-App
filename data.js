8693591b6bf9e9d520677d43bd2c8e0bb12bad56
Salome Saghirishvili <sagirishvilis@gmail.com>
2023-10-10 18:42:53 +0400

    .

--- data.js
+++ data.js
@@ -1,21 +1,6 @@
 const tasks = [
   {
-    description: "task-0",
+    description: "Hello!",
     active: true,
-  },
-
-  {
-    description: "task-1",
-    active: false,
-  },
-
-  {
-    description: "task-2",
-    active: true,
-  },
-
-  {
-    description: "task-3",
-    active: false,
   },
 ];

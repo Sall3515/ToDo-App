a4dc5ba53603a5d714fc850da5b32d34668a2d19
Salome Saghirishvili <sagirishvilis@gmail.com>
2023-10-10 17:01:43 +0400

    add checkmark

--- setup.js
+++ setup.js
@@ -2,13 +2,14 @@ const setup = (tasks, rootElementId) => {
   let taskList = "";
   for (let i = 0; i < tasks.length; i++) {
     let task = tasks[i];
-    taskList += `<div class="task" id="${task.id}" data-id="${i}">
+    taskList += `<div  class="task"  data-id="${i}"  
+    }>
           <div class="text task-text">
-            <input class="list-checkbox" type="checkbox" ${
-              task.active ? "" : "checked"
-            } >
-            <label></label>
-            <span class="taskname" }>
+            <input  id="task-${i}" class="list-checkbox" type="checkbox" ${
+      task.active ? " " : "checked"
+    } >
+            <label for="task-${i}"></label>
+            <span class="taskname ${task.active ? " " : "task-completed"}" >
               ${task.description}
             </span>
           </div>

210013a0302950fffaa3fd73d2c9bb8d1828d3fc
Salome Saghirishvili <sagirishvilis@gmail.com>
2023-10-10 18:32:43 +0400

    add delete button

--- setup.js
+++ setup.js
@@ -2,21 +2,47 @@ const setup = (tasks, rootElementId) => {
   let taskList = "";
   for (let i = 0; i < tasks.length; i++) {
     let task = tasks[i];
-    taskList += `<div  class="task"  data-id="${i}"  
-    }>
+    taskList += `<div  class="task"  data-id="${i}" }>
           <div class="text task-text">
-            <input  id="task-${i}" class="list-checkbox" type="checkbox" ${
-      task.active ? " " : "checked"
-    } >
+            <input  id="task-${i}" class="list-checkbox" type="checkbox"
+             ${task.active ? " " : "checked"} >
             <label for="task-${i}"></label>
             <span class="taskname ${task.active ? " " : "task-completed"}" >
               ${task.description}
             </span>
           </div>
-          <span onclick="deleteTask(listContainer)" class="x">
-            X
-          </span>
+          <span class="x">
+            X</span>
         </div>`;
   }
+
   document.getElementById(rootElementId).innerHTML = taskList;
+
+  const taskListElement = document.getElementById("tasks");
+  const checkItems = Array.from(taskListElement.querySelectorAll(".text"));
+
+  checkItems.map((checkItem) => {
+    checkItem.addEventListener("click", (event) => {
+      let id = Number(
+        event.target.parentNode.parentNode.getAttribute("data-id")
+      );
+
+      tasks[id].active = !tasks[id].active;
+
+      setup(tasks, "tasks");
+    });
+  });
+
+  const deleteButtons = Array.from(taskListElement.querySelectorAll(".x"));
+
+  deleteButtons.map((deleteButton) => {
+    deleteButton.addEventListener("click", (event) => {
+      let id = Number(event.target.parentNode.getAttribute("data-id"));
+
+      let head = tasks.slice(0, id);
+      let tail = tasks.slice(id + 1, tasks.length);
+      // console.log(id);
+      setup([...head, ...tail], "tasks");
+    });
+  });
 };
